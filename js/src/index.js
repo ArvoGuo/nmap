089377dceb4f9e0a6819fa2832d763b5260bf28e
Arvo <ArvoGuo@gmail.com>
2015-11-27 12:24:44 +0800

    version: 1.0.10 容错 地图未加载

--- js/src/index.js
+++ js/src/index.js
@@ -2,37 +2,39 @@
 if (typeof window.qq.maps.Map === 'undefined') {
   console.log('qq map is not found');
   module.exports = {};
-  return;
+
+} else {
+  var CoreShape = require('./shape');
+  var _ = require('lodash');
+
+  var NMap = {};
+
+  NMap.all = [];
+
+  NMap.create = function(type) {
+    var args = Array.prototype.slice.call(arguments);
+    var s = new (CoreShape[type].bind.apply(CoreShape[type], args));
+
+    NMap.all.push(s);
+    return s;
+  };
+
+  NMap.addFn = function(type, key, fn) {
+    if (!CoreShape[type]) {
+      throw new Error( type + " isn't exist" );
+    }
+    if (CoreShape[type].prototype[key]) {
+      console.warn(type + 'has key' + key + ', you rewrite it;');
+    }
+    CoreShape[type].prototype[key] = fn;
+  };
+
+
+
+
+
+  module.exports = NMap;
+
+
+
 }
-var CoreShape = require('./shape');
-var _ = require('lodash');
-
-var NMap = {};
-
-NMap.all = [];
-
-NMap.create = function(type) {
-  var args = Array.prototype.slice.call(arguments);
-  var s = new (CoreShape[type].bind.apply(CoreShape[type], args));
-
-  NMap.all.push(s);
-  return s;
-};
-
-NMap.addFn = function(type, key, fn) {
-  if (!CoreShape[type]) {
-    throw new Error( type + " isn't exist" );
-  }
-  if (CoreShape[type].prototype[key]) {
-    console.warn(type + 'has key' + key + ', you rewrite it;');
-  }
-  CoreShape[type].prototype[key] = fn;
-};
-
-
-
-
-
-module.exports = NMap;
-
-
