e00e9bc0338a5d9250d27ca2b821fcd63378dd99
Arvo <ArvoGuo@gmail.com>
2016-01-19 12:18:21 +0800

    chore

--- publish.sh
+++ publish.sh
@@ -2,5 +2,8 @@
 echo "hahahahahahahahahhahahaha";
 npm version patch ;
 npm publish;
-nnpm publish;
+npm --registry=http://npm-registry.napos.io\
+  --cache=$HOME/.npm/.cache/nnpm \
+  --disturl=http://npm-registry.npdev.io/dist \
+  --userconfig=$HOME/.nnpmrc;
 
