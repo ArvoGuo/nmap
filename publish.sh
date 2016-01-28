f34fdaaa0345c3a31dd04b75d96256bab620a123
Arvo <ArvoGuo@gmail.com>
2016-01-28 16:52:40 +0800

    fix: sh

--- publish.sh
+++ publish.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "hahahahahahahahahhahahaha";
+git add .;
 npm version patch ;
 npm publish;
 npm publish --registry=http://npm-registry.napos.io --cache=$HOME/.npm/.cache/nnpm --disturl=http://npm-registry.npdev.io/dist --userconfig=$HOME/.nnpmrc;
