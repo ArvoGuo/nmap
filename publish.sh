3ea90ed69a7dc834f47aea48fb664856c29a8fa0
Arvo <ArvoGuo@gmail.com>
2016-02-22 16:05:37 +0800

    feat: name

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 echo "hahahahahahahahahhahahaha";
 npm version patch ;
 # npm publish;
-# npm publish --registry=http://npm-registry.napos.io --cache=$HOME/.npm/.cache/nnpm --disturl=http://npm-registry.npdev.io/dist --userconfig=$HOME/.nnpmrc;
+npm publish --registry=http://npm-registry.napos.io --cache=$HOME/.npm/.cache/nnpm --disturl=http://npm-registry.npdev.io/dist --userconfig=$HOME/.nnpmrc;
 npm publish --registry=http://r.tools.elenet.me --cache=$HOME/.npm/.cache/nnpm --disturl=http://npm-registry.npdev.io/dist --userconfig=$HOME/.nnpmrc;
 
 
